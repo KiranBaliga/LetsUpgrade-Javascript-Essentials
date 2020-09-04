2a6dcb84ae4c4f6a516380bf5ddfe3516aed975f
Kiran Baliga B <68471170+KiranBaliga@users.noreply.github.com>
2020-09-04 10:24:27 +0530

    Create Q4.js

---
+++ Day 2/Q4.js
@@ -0,0 +1,10 @@
+function contains(arr, element) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === element) {
+            return true;
+        }
+    }
+    return false;
+}
+arr = [a, w, e, n];
+console.log(contains(arr, a));
