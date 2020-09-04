872dd2fd79e3903588852ac5722fceecc91a3ab1
Kiran Baliga B <68471170+KiranBaliga@users.noreply.github.com>
2020-09-04 10:25:53 +0530

    Create Q5.js

---
+++ Day 2/Q5.js
@@ -0,0 +1,26 @@
+<script> 
+
+function func() { 
+
+  
+
+    // Original Array 
+
+    var arr = ['Portal', 'Science',  
+
+               'Computer', 'letsupgrade']; 
+
+    document.write(arr); 
+
+  
+
+    // Reversed array 
+
+    var new_arr = arr.reverse(); 
+
+    document.write("<br>"); 
+
+    document.write(new_arr); 
+} 
+func(); 
+</script>          
