c00505e3fb5d9613888a9fc31f294acf0c1c42e8
Kiran Baliga B <68471170+KiranBaliga@users.noreply.github.com>
2020-09-04 10:19:45 +0530

    Create Q3.js

---
+++ Day 2/Q3.js
@@ -0,0 +1,22 @@
+<script> 
+
+    // input array contain some elements. 
+
+    var array = [-10, -0.20, 0.30, -40, -50]; 
+
+  
+
+    // method (return element > 0). 
+
+    var found = array.find(function (element) { 
+
+        return element > 0; 
+
+    }); 
+
+  
+
+    // Printing desired values. 
+
+    document.write(found); 
+</script> 
